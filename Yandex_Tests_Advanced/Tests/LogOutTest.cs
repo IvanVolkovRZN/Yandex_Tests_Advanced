d71a05499b3af0d272346b77abe4bc2361f01107
Иван Волков <ivan.volkov.rzn@gmail.com>
2018-05-17 11:25:24 +0300

    готово

--- Yandex_Tests_Advanced/Tests/LogOutTest.cs
+++ Yandex_Tests_Advanced/Tests/LogOutTest.cs
@@ -4,20 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Yandex_Tests_Advanced.BisnesObject;
 using Yandex_Tests_Advanced.PO;
 
 namespace Yandex_Tests_Advanced.Tests
 {
     class LogOutTest : AbstractTest
     {
+        User user = new User("kirukato203@yandex.ru", "Rjcnz35610");
 
-        
 
         [Test]
         public void LogOut()
         {
             MailPage mailPage = new MailPage();         
-            LoginApp("kirukato203@yandex.ru", "Rjcnz35610");
+            LoginApp(user);
             SearchPage searchPage = mailPage.LogOutMail();
 
             String buttonTextActual = searchPage.ButtonTextActual();
